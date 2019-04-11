b5fda2328245d1a3c12892fc5c88becb99c21458
Bogyeong Kim <rlaqhrud852@gmail.com>
2019-04-12 03:08:15 +0900

    BasicShaderFramework

---
+++ shader_framework/shader_framework/ShaderFramework.cpp
@@ -0,0 +1,32 @@
+#include "ShaderFramework.h"	
+#include <stdio.h>
+
+//---------------------------
+// 전역 함수
+//---------------------------
+
+//D3D 관련
+LPDIRECT3D9			gpD3D			= NULL;		//D3D
+LPDIRECT3DDEVICE9	gpD3DDevice		= NULL;		//D3D 장치
+
+//폰트
+ID3DXFont*			gpFont			= NULL;
+
+//모델
+
+//쉐이더
+
+//텍스쳐
+
+//프로그램 이름
+const char*			gAppName		= "초간단 쉐이더 데모 프레임 워크";
+
+
+//----------------------------------
+// 프로그램 진입점 / 메세지 루프
+//----------------------------------
+
+INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, INT)
+{
+
+}
