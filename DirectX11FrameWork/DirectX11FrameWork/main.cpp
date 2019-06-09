e45008396460e40cdb21409ecc995b85797c018a
Bogyeong Kim <rlaqhrud852@gmail.com>
2019-06-09 19:17:34 +0900

    framework revision

--- DirectX11FrameWork/DirectX11FrameWork/main.cpp
+++ DirectX11FrameWork/DirectX11FrameWork/main.cpp
@@ -1,8 +1,8 @@
-#include <d3d11.h>
-#include <iostream>
 #include <Windows.h>
+#include <iostream>
+#include <d3d11.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
+INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, INT)
 {
 	return 0;
 }
